7631f47fa010c135bdbe412d84cdc06979788824
b1u3c1ph3r <shuler.david.p@gmakil.com>
2013-03-20 21:36:11 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Firstblood::Application.config.secret_token = 'bd62bb1c5a72540e9738d675ad8315f29769bc69a9f6f7b5de5f9fb0d08825e1c8e46c254e681e9d5d72d83e709e93b3e8c797814bbeef3e362dc1f96c553095'
